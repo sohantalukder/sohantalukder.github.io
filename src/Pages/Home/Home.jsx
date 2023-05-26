9e2fdd533c9aa1b1e63da766b8e74f8efb8edeb4
sohantalukder <mdtalukder.sohan@gmail.com>
2023-05-26 18:18:27 +0600

    add education section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Sections/Hero";
+import Education from "./Sections/Education";
 
 const Home = () => {
     return (
-        <div className='col-span-12 lg:col-span-7 xl:col-span-9'>
+        <div className='col-span-12 lg:col-span-7 xl:col-span-9  px-5 md:px-0'>
             <Hero />
+            <Education />
         </div>
     );
 };
