70d35637c9f539b1b85d36711bc9c9ade6e358a8
sohantalukder <mdtalukder.sohan@gmail.com>
2023-05-27 01:41:55 +0600

    change copyright design

--- src/Components/CopyRight/CopyRight.jsx
+++ src/Components/CopyRight/CopyRight.jsx
@@ -3,9 +3,9 @@ import { Icon } from "../Icon/Icon";
 
 const CopyRight = () => {
     return (
-        <div className='py-4 flex justify-center bg-dark1 mt-12 lg:mt-16 space-x-5'>
+        <div className='py-4 flex justify-center bg-dark1 mt-12 lg:mt-16 space-x-3 items-center md:space-x-5 text-center'>
             <Icon name={"Copy"} className='fill-white' />
-            <p className='text-white'>
+            <p className='text-white text-center'>
                 2023 ALl Rights Reserved. Sohan Talukder
             </p>
         </div>
