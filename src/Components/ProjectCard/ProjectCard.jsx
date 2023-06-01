6946d967c7780abb66f4a1d4c3e12c5dd3875ff2
sohantalukder <mdtalukder.sohan@gmail.com>
2023-06-02 00:44:35 +0600

    add link in project link

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -19,11 +19,11 @@ const ProjectCard = ({ project = {}, index }) => {
     return (
         <>
             <div
-                className={`flex flex-col lg:flex-row rounded-lg bg-dark1 h-[50rem] gap-6 m-0 w-auto xl:h-[27rem] xl:p-8  ${
-                    index % 2 === 0 && "flex-row-reverse"
+                className={`flex flex-col  rounded-lg bg-dark1 h-[35rem] sm:h-[40rem] lg:h-[45rem] gap-6 m-0 w-auto xl:h-[27rem] p-8  ${
+                    index % 2 !== 0 ? "xl:flex-row" : "xl:flex-row-reverse"
                 }`}
             >
-                <div className='w-full xl:w-[45rem] h-auto rounded-lg overflow-hidden'>
+                <div className='w-full xl:w-[45rem] md:h-[25rem] lg:h-[30rem] xl:h-auto rounded-lg overflow-hidden'>
                     <a target='_blank' href={demo} rel='noreferrer'>
                         <img
                             src={image}
@@ -39,14 +39,14 @@ const ProjectCard = ({ project = {}, index }) => {
                         />
                     </a>
                 </div>{" "}
-                <div className=' flex w-full m-0  sm:w-[30rem] h-auto flex-col text-center justify-center'>
+                <div className=' flex w-full m-0  xl:w-[30rem] h-auto flex-col text-center justify-center'>
                     <h3 className='text-lg xl:text-xl font-semibold text-gray mb-2'>
                         {name} {icon}
                     </h3>
                     <p className='text-text text-sm xl:text-base'>
                         {description}
                     </p>
-                    <div className='flex flex-row justify-center items-center gap-2 mt-4'>
+                    <div className='flex flex-row justify-center items-center gap-2 mt-4 flex-wrap'>
                         {technology?.map((item, index) => (
                             <div
                                 className='bg-gray text-sm xl:text-base rounded-sm px-1 py-0.5 text-text'
@@ -56,35 +56,55 @@ const ProjectCard = ({ project = {}, index }) => {
                             </div>
                         ))}
                     </div>
-                    <div className='mt-4 flex gap-3 justify-center'>
+                    <div className='mt-4 flex gap-3 justify-center flex-wrap'>
                         {demoAPP && (
-                            <Link className='text-gray '>
+                            <a
+                                target='_blank'
+                                href={demoAPP}
+                                className='text-gray '
+                            >
                                 <span>Demo App</span> 📱
-                            </Link>
+                            </a>
                         )}
                         {demo && (
-                            <Link className='text-gray flex items-center gap-1'>
+                            <a
+                                target='_blank'
+                                href={demo}
+                                className='text-gray flex items-center gap-1'
+                            >
                                 <span>Live Demo</span> <BsLink45Deg />
-                            </Link>
+                            </a>
                         )}
                         {codeLink?.map((item, index) => {
                             const { frontEnd, backEnd } = item || {};
                             return (
                                 <Fragment key={index}>
                                     {!frontEnd && !backEnd && (
-                                        <Link className='text-gray flex items-center gap-1'>
+                                        <a
+                                            target='_blank'
+                                            href={item}
+                                            className='text-gray flex items-center gap-1'
+                                        >
                                             Code <BsGithub />
-                                        </Link>
+                                        </a>
                                     )}
                                     {frontEnd && (
-                                        <Link className='text-gray flex items-center gap-1'>
+                                        <a
+                                            target='_blank'
+                                            href={frontEnd}
+                                            className='text-gray flex items-center gap-1'
+                                        >
                                             Frontend Code <BsGithub />
-                                        </Link>
+                                        </a>
                                     )}
                                     {backEnd && (
-                                        <Link className='text-gray flex items-center gap-1'>
+                                        <a
+                                            target='_blank'
+                                            href={backEnd}
+                                            className='text-gray flex items-center gap-1'
+                                        >
                                             Backend Code <BsGithub />
-                                        </Link>
+                                        </a>
                                     )}
                                 </Fragment>
                             );
